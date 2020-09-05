1ec1b0e7ee0931f3569d98d9d2bef74328d16ca4
hirokihello <iammyeye1@gmail.com>
2020-09-05 23:42:49 +0900

    feature: add webpack and basic files

---
+++ index.js
@@ -0,0 +1,7 @@
+import utils from './utils';
+
+window.onload = function() {
+  alert('index.js loaded!!!')
+};
+
+utils();
---
+++ utils.js
@@ -0,0 +1,4 @@
+export default function utils() {
+  console.log("utils was loaded")
+  //
+}
